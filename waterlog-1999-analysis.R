24604fbcb5d75ad2957df0c56a88423c24a2648c
s-mxwll <simonjosephmaxwell@gmail.com>
2021-03-06 19:02:34 +0000

    added weighted log odds and sentiment analysis

--- waterlog-1999-analysis.R
+++ waterlog-1999-analysis.R
@@ -4,6 +4,7 @@
 
 # Load packages:
 library(glue)
+library(tidylo)
 library(rvest)
 library(scales)
 library(tidytext)
@@ -98,6 +99,7 @@ waterlog_tidy %>%
          chapter_number = fct_reorder(chapter_number, n)) %>% 
   ggplot(aes(n, chapter_number)) + 
   geom_col(fill = "#006994") +
+  scale_x_continuous(labels = comma_format()) +
   labs(title = "The longest chapters include swims in the River Test and River Fowey",
        subtitle = "Number of words in each chapter",
        x = n_label,
@@ -113,6 +115,7 @@ waterlog_tidy %>%
          chapter_combined = fct_reorder(chapter_combined, n)) %>% 
   ggplot(aes(n, chapter_combined)) + 
   geom_col(fill = "#006994") +
+  scale_x_continuous(labels = comma_format()) +
   labs(title = "The longest chapters include swims in the River Test and River Fowey",
        subtitle = "Number of words in each chapter",
        x = n_label,
@@ -128,6 +131,7 @@ waterlog_tidy %>%
   mutate(word = fct_reorder(word, n)) %>% 
   ggplot(aes(n, word, fill = is_water)) +
   geom_col() +
+  scale_x_continuous(labels = comma_format()) +
   scale_fill_manual(values = c("#006994", "#800000")) +
   labs(title = "With one exception, the most common words are stop words",
        subtitle = "Top 20 most common words",
@@ -172,7 +176,7 @@ waterlog_tidy %>%
   geom_col(fill = "#006994") +
   labs(title = "Which chapters are the wettest?",
        subtitle = "Number of times water appears in each chapter",
-       x = "Chapter",
+       x = "Chapter number",
        y = "n(water)")
 
 # Percentage of words that are water by chapter:
@@ -198,6 +202,35 @@ waterlog_tidy %>%
        y = NULL,
        caption = caption)
 
+# Weighted log odds ratio for water through the book:
+waterlog_tidy %>% 
+  anti_join(stop_words, by = "word") %>% 
+  count(chapter_number, word, sort = TRUE) %>% 
+  bind_log_odds(chapter_number, word, n) %>% 
+  arrange(desc(log_odds_weighted)) %>% 
+  filter(word == "water") %>% 
+  ggplot(aes(chapter_number, log_odds_weighted)) +
+  geom_col(fill = "#006994") +
+  geom_smooth(method = "lm", colour = "#942F00") +
+  labs(title = "In which chapters is the word water more or less likely to appear?",
+       subtitle = "The word water becomes a more common feature through the book",
+       x = "Chapter number",
+       y = "Weighted log odds ratio")
+
+# Weighted log odds ratio in descending order:
+waterlog_tidy %>% 
+  anti_join(stop_words, by = "word") %>% 
+  count(chapter_number, word, sort = TRUE) %>% 
+  bind_log_odds(chapter_number, word, n) %>% 
+  filter(word == "water") %>% 
+  mutate(chapter_number = fct_reorder(as.factor(chapter_number), log_odds_weighted)) %>% 
+  ggplot(aes(log_odds_weighted, chapter_number)) +
+  geom_col(fill = "#006994") +
+  labs(title = "In which chapters is the word water more or less likely to appear?",
+       subtitle = "Weighted log odds ratio of the word water",
+       x = "Weighted log odds ratio",
+       y = "Chapter number")
+
 # Scrape text from A Guide to the 58 Crazy Different Terms for Water:
 bow_scraped <- read_html("https://www.atlasobscura.com/articles/a-guide-to-the-58-crazy-different-terms-for-water") %>% 
   html_nodes(".item-body-text-graf") %>% 
@@ -208,12 +241,14 @@ bow <- tibble(text = bow_scraped) %>%
   unnest_tokens(bow, text, token = "ngrams", n = 2) %>% 
   filter(str_detect(bow, "^[^\\s]+\\s\\d+$")) %>% 
   mutate(bow = str_remove_all(bow, pattern = " \\d+")) %>% 
-  filter(bow != "nearly") %>% 
+  filter(bow != "nearly",
+         !str_detect(bow, "sea")) %>% 
   add_row(bow = c("moat",
                   "gill",
                   "lido",
                   "moat",
                   "llyn",
+                  "sea",
                   "whirlpool"))
 
 # Count water bodies mentioned in text:
@@ -230,30 +265,93 @@ waterlog_tidy %>%
        y = "Water body",
        caption = caption)
 
-# Which words most commonly precede water?
-water_before <- bigrams_filtered %>%
-  filter(word2 == "water") %>%
-  count(word1, sort = TRUE) %>% 
-  top_n(10) %>% 
-  mutate(ba = "[] water") %>% 
-  rename(word = word1)
+# SENTIMENT ANALYSIS ------------------------------------------------------
 
-# Which words most commonly follow water?
-water_after <- bigrams_filtered %>%
-  filter(word1 == "water") %>%
-  count(word2, sort = TRUE) %>% 
-  top_n(10) %>% 
-  mutate(ba = "water []") %>% 
-  rename(word = word2)
+# Sentiment score for each 80-line chunk:
+waterlog_tidy %>% 
+  inner_join(get_sentiments("bing")) %>%
+  count(chapter_number, index = line_number %/% 80, sentiment) %>%
+  spread(sentiment, n, fill = 0) %>% # needs to be pivot_wider
+  mutate(sentiment = positive - negative) %>% 
+  ggplot(aes(index, sentiment, fill = sentiment)) +
+  geom_col(show.legend = FALSE) +
+  scale_fill_gradient(low = "#006994",
+                      high = "#942F00") +
+  labs(title = "73% of 80-line chunks are emotionally positive",
+       subtitle = "Hell Gill and Corryvreckan Whirlpool are the msot emotionally negative",
+       x = "80-line chunk",
+       y = "Sentiment score")
 
-# What are the most common words which occur before and after water?
-water_before %>% 
-  bind_rows(water_after) %>% 
-  mutate(word = fct_reorder(word, n)) %>% 
-  ggplot(aes(n, word)) +
-  geom_col(fill = "#006994") +
-  facet_wrap(~ba, scales = "free_y") +
-  labs(title = "Green, white, black, blue",
-       subtitle = "Most common words to precede and follow water",
-       x = n_label,
+# Sentiment score for each chapter:
+waterlog_tidy %>% 
+  inner_join(get_sentiments("bing")) %>%
+  count(chapter_number, sentiment) %>%
+  pivot_wider(names_from = sentiment,
+              values_from = n,
+              values_fill = 0) %>% 
+  mutate(sentiment = positive - negative) %>% 
+  arrange(sentiment)
+  ggplot(aes(chapter_number, sentiment, fill = sentiment)) +
+  geom_col(show.legend = FALSE) +
+  scale_fill_gradient(low = "#006994",
+                      high = "#942F00") +
+  labs(title = "72% of chapters (including the first 16 chapters) are emotionally positive",
+       subtitle = "Sentiment score by chapter number",
+       x = "Chapter",
+       y = "Sentiment score")
+
+# Get AFINN score for each 80-line chunk:
+afinn <- waterlog_tidy %>% 
+  inner_join(get_sentiments("afinn")) %>% 
+  group_by(index = line_number %/% 80) %>% 
+  summarise(sentiment = sum(value)) %>% 
+  mutate(method = "AFINN")
+
+# Get Bing et al. and NRC sentiment score:
+bing_and_nrc <- bind_rows(
+  waterlog_tidy %>% 
+    inner_join(get_sentiments("bing")) %>%
+    mutate(method = "Bing"),
+  waterlog_tidy %>% 
+    inner_join(get_sentiments("nrc") %>% 
+                 filter(sentiment %in% c("positive", 
+                                         "negative"))
+    ) %>%
+    mutate(method = "NRC")) %>%
+  count(method, index = line_number %/% 80, sentiment) %>%
+  pivot_wider(names_from = sentiment,
+              values_from = n,
+              values_fill = 0) %>% 
+  mutate(sentiment = positive - negative)
+
+# Bind sentiment score for each method together and plot:
+bind_rows(afinn, 
+          bing_and_nrc) %>%
+  ggplot(aes(index, sentiment, fill = method)) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~method, ncol = 1, scales = "free_y") +
+  labs(title = NULL,
+       subtitle = "Sentiment score according to three different methods",
+       x = "80-line chunk",
+       y = "Sentiment score")
+
+# What are the most positive and negative words?
+bing_word_count <- waterlog_tidy %>%
+  inner_join(get_sentiments("bing")) %>%
+  count(word, sentiment, sort = TRUE) %>%
+  ungroup()
+
+# Which words contribute more to sentiment score?
+bing_word_count %>% 
+  group_by(sentiment) %>% 
+  top_n(20) %>% 
+  ungroup() %>% 
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(n, word, fill = sentiment)) +
+  geom_col(show.legend = FALSE) +
+  scale_fill_manual(values = c("#006994", "#800000")) +
+  facet_wrap(~sentiment, scales = "free_y") +
+  labs(title = "Which words contribute more to sentiment score?",
+       subtitle = "Simile is a common figure of speech in Roger Deakin's writing",
+       x = "Contribution to sentiment",
        y = NULL)
